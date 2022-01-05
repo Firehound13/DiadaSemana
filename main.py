39f652d591b85a2c3245bed0edf4119a1083fd71
Firehound13 <leaobr101@gmail.com>
2022-01-05 12:53:52 -0300

    Exercício finalizado

---
+++ main.py
@@ -0,0 +1,18 @@
+numero = int(input("Digite um número!Este deve corresponder a um dia da semana."))
+
+if numero == 1:
+    print("Hoje é Domingo")
+elif numero == 2:
+    print("Hoje é Segunda")
+elif numero == 3:
+    print("Hoje é Terça")
+elif numero == 4:
+    print("Hoje é Quarta")
+elif numero == 5:
+    print("Hoje é Quinta")
+elif numero == 6:
+    print("Hoje é Sexta")
+elif numero == 7:
+    print("Hoje é Sábado")
+else:
+    print("Valor inválido! Tente um número de 1 a 7.")
